4f31e25c238e162e7840bfe5ca7a09d442e3b6d3
Vinayak Malik <vinayakmalik1999@gmail.com>
2025-09-05 23:36:21 -0400

    up till inclusion

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import argparse
+import base64
+import json
+import logging as log
+
+import requests
 from util import extract_public_key, verify_artifact_signature
 from merkle_proof import DefaultHasher, verify_consistency, verify_inclusion, compute_leaf_hash
 
+# //------ NOTE: I have used Pycharm's autocomplete code function
+# in a lot of places as well as stackoverflow in the code however the logic is all mine -----//
+
+bundle_file_path='artifact.bundle'
+checkpoint_file_path='checkpoint.json'
+# NOTE: verify this to make sure, but extracting log index first from the artifact.bundle
+def get_log_index_from_bundle():
+    # Assuming file path remains the same
+    with open(bundle_file_path, 'r') as file:
+        dump = json.load(file)
+        try:
+            log_index = dump.get('rekorBundle').get('Payload').get('logIndex')
+            return log_index
+        except KeyError as e:
+            log.error(e)
+            return None
+
 def get_log_entry(log_index, debug=False):
     # verify that log index value is sane
-    pass
+    if log_index is None:
+        log.info("log_index cannot be empty")
+        return
+    log_entry = {}
+    try:
+        log_entry = requests.get(f'https://rekor.sigstore.dev/api/v1/log/entries?logIndex={log_index}').json()
+        return log_entry
+
+    except requests.exceptions.RequestException as ex:
+        log.error('upstream response error from sigstore:',ex)
+        return None
 
 def get_verification_proof(log_index, debug=False):
     # verify that log index value is sane
@@ -12,14 +44,35 @@ def get_verification_proof(log_index, debug=False):
 
 def inclusion(log_index, artifact_filepath, debug=False):
     # verify that log index and artifact filepath values are sane
-    # extract_public_key(certificate)
-    # verify_artifact_signature(signature, public_key, artifact_filepath)
+    log_entry = get_log_entry(log_index,debug)
+    raw_obj = log_entry[list(log_entry.keys())[0]]
+    body_encoded = raw_obj.get('body')
+    body = json.loads(base64.b64decode(body_encoded))
+    signature = base64.b64decode(body["spec"]["signature"]["content"])
+    cert = base64.b64decode(body["spec"]["signature"]["publicKey"]["content"])
+    public_key = extract_public_key(cert)
+    verify_artifact_signature(signature, public_key, artifact_filepath)
     # get_verification_proof(log_index)
     # verify_inclusion(DefaultHasher, index, tree_size, leaf_hash, hashes, root_hash)
-    pass
 
 def get_latest_checkpoint(debug=False):
-    pass
+    # again check this but retreiving log_index first from file
+    log_index = get_log_index_from_bundle()
+    if log_index is None:
+        log.info("log_index cannot be empty")
+        return
+    try:
+        checkpoint = requests.get(f'https://rekor.sigstore.dev/api/v1/log?logIndex={log_index}').json()
+        # store output in checkpoint.json
+        with open(checkpoint_file_path, 'w') as file:
+            # NOTE: maybe no need to json convert look later
+            json.dump(checkpoint,file)
+
+        return checkpoint
+
+    except requests.exceptions.RequestException as ex:
+        log.error('upstream response error from sigstore:',ex)
+        return None
 
 def consistency(prev_checkpoint, debug=False):
     # verify that prev checkpoint is not empty
@@ -54,6 +107,7 @@ def main():
     args = parser.parse_args()
     if args.debug:
         debug = True
+        get_log_entry(475366375)
         print("enabled debug mode")
     if args.checkpoint:
         # get and print latest checkpoint from server
